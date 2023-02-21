3b5f357f49523085122e2b6e8a58d76ee98021c7
Hoeven-Pim <Hoeven.Pim@bcg.com>
2023-02-21 13:05:17 +0100

    Added filtered summary function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 
 import causalgraphicalmodels
 import daft
+import numpyro.diagnostics
 import matplotlib.pyplot as plt
 
 
@@ -15,3 +16,9 @@ def draw_dag(dag: causalgraphicalmodels.CausalGraphicalModel, coordinates: dict)
     with plt.rc_context({"figure.constrained_layout.use": False}):
         pgm.render()
     plt.gca().invert_yaxis()
+
+
+def print_filtered_summary(samples, prob=0.9, group_by_chain=True):
+    filtered_samples = {name: values for name, values in samples.items() if len(values.shape) == 1}
+    numpyro.diagnostics.print_summary(filtered_samples, prob=prob, group_by_chain=group_by_chain)
+
